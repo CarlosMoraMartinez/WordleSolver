5d004f13eeb5a724c0ae999522373f571b05f3bd
Carlos Mora <carmoma9@gmail.com>
2022-03-11 23:33:43 +0100

    First commit: Wordle works. Solver tries random words

--- solve.py
+++ solve.py
@@ -4,22 +4,25 @@ from wordle import Wordle
 N_GAMES = 1000
 
 class WSolver():
-    def __init__(self, game):
+    def __init__(self, game, configure = 1):
         self.game = game
         self.words = game.words
+        configs = {1: self.playgame_random}
+        setattr(self, "playgame", configs[configure])
 
     def chooseWord(self):
         word = sample(self.words, 1).pop()
         print(f"Trying {word}")
         return word
-
-    def playgame(self):
+    
+    def playgame_random(self):
         self.game.reset()
         has_won = False
         while(self.game.canTry() and not has_won):
             word = self.chooseWord()
             n, has_won = self.game.tryWord(word)
         return n, has_won    
+
 
 def main():
     game = Wordle()
